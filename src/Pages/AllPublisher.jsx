26e1cb968f6de5188d8d1832858ba8b2824a49c5
unknown <emonhossen1000@gmail.com>
2023-12-04 22:36:24 +0600

    all publisher added

--- src/Pages/AllPublisher.jsx
+++ src/Pages/AllPublisher.jsx
@@ -1,11 +1,24 @@
- 
+import { useLoaderData } from "react-router-dom";
+import SectionTitle from "../Hooks/SectionTitle";
 
 const AllPublisher = () => {
-    return (
-        <div>
-            this is publisher page
-        </div>
-    );
+  const datas = useLoaderData();
+
+  return (
+    <>
+      <SectionTitle heading="All Publisher"></SectionTitle>
+      {datas.map((data) => (
+        <>
+          <div className="avatar flex-col ml-5 gap-5 justify-center text-center">
+            <div className="w-24 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
+              <img src={data.image} />
+            </div>
+            <p>{data.publisher}</p>
+          </div>
+        </>
+      ))}
+    </>
+  );
 };
 
 export default AllPublisher;
--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -3,19 +3,19 @@ import Main from "../Layout/Main";
 import ErrorPage from "../Pages/ErrorPage";
 import Home from "../Pages/Home";
 
- const router=createBrowserRouter([
-    {
-        path:"/",
-        element:<Main></Main>,
-        errorElement:<ErrorPage></ErrorPage>,
-        children:[
-            {
-                path:"/",
-                element:<Home></Home>,
-                loader:()=>fetch("data.json")
-            }
-        ]
-    }
- ])
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main></Main>,
+    errorElement: <ErrorPage></ErrorPage>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+        loader: () => fetch("http://localhost:5000/articles"),
+      },
+    ],
+  },
+]);
 
 export default router;
