cde4f4cea077bc28691aff3976f08ebd8cb90642
unknown <emonhossen1000@gmail.com>
2024-01-18 18:26:50 +0600

    delete addded succesfully

--- src/Components/MyArticle.jsx
+++ src/Components/MyArticle.jsx
@@ -5,6 +5,7 @@ import Navbar from "./Navbar";
 import { AuthContext } from "../Providers/AuthProvider";
 import { MdBrowserUpdated, MdDeleteForever } from "react-icons/md";
 import { Link } from "react-router-dom";
+import Swal from "sweetalert2";
 
 const MyArticle = () => {
   const { user } = useContext(AuthContext);
@@ -17,6 +18,41 @@ const MyArticle = () => {
       .then((res) => res.json())
       .then((data) => setArticle(data));
   }, [url]);
+
+  // delete
+  const handleDelete = (id) => {
+    console.log(id);
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        fetch(`http://localhost:5000/allArticles/${id}`, {
+          method: "DELETE",
+        })
+          .then((res) => res.json())
+          .then((data) => {
+            console.log(data);
+            if (data.deletedCount > 0) {
+              Swal.fire(
+                "Deleted!",
+                "Your product has been deleted.",
+                "success"
+              );
+
+              // **************** here remove from ui
+              const remaining = article.filter((coffee) => coffee._id !== id);
+              setArticle(remaining);
+            }
+          });
+      }
+    });
+  };
 
   return (
     <div>
@@ -38,29 +74,29 @@ const MyArticle = () => {
           </thead>
           <tbody>
             {article?.map((item, idx) => (
-              <>
-                <tr>
-                  <th>{idx + 1}</th>
-                  <td>
-              <Link  to={`/details/${item._id}`}>
-              <button className="btn btn-outline btn-warning">Details</button>
-              </Link>
-                  </td>
-                  <td>{item.name}</td>
-                  <td> ststus </td>
-                  <td> premium</td>
-                  <td>
-                    <button>
-                      <MdBrowserUpdated className="text-3xl text-sky-500"></MdBrowserUpdated>
+              <tr key={item._id}>
+                <th>{idx + 1}</th>
+                <td>
+                  <Link to={`/details/${item._id}`}>
+                    <button className="btn btn-outline btn-warning">
+                      Details
                     </button>
-                  </td>
-                  <th>
-                    <button>
-                      <MdDeleteForever className="text-4xl text-red-600"></MdDeleteForever>
-                    </button>
-                  </th>
-                </tr>
-              </>
+                  </Link>
+                </td>
+                <td>{item.name}</td>
+                <td> ststus </td>
+                <td> premium</td>
+                <td>
+                  <button>
+                    <MdBrowserUpdated className="text-3xl text-sky-500"></MdBrowserUpdated>
+                  </button>
+                </td>
+                <th>
+                  <button onClick={() => handleDelete(item._id)}>
+                    <MdDeleteForever className="text-4xl text-red-600"></MdDeleteForever>
+                  </button>
+                </th>
+              </tr>
             ))}
           </tbody>
         </table>
